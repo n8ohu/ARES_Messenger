01af888af3aad46721833067d1b973d60e3a0f10
Matthew <n8ohu@yahoo.com>
2015-01-02 22:49:18 -0500

    First Commit to Github
    Open Source Messaging and Chat client for ARDOP

---
+++ ARES_Messenger/Main.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace ARES_Messenger
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbReceiveWindow = new System.Windows.Forms.RichTextBox();
+            this.rtbTransmitWindow = new System.Windows.Forms.RichTextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.MenuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.messageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.attachmentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtbReceiveWindow
+            // 
+            this.rtbReceiveWindow.Location = new System.Drawing.Point(168, 27);
+            this.rtbReceiveWindow.Name = "rtbReceiveWindow";
+            this.rtbReceiveWindow.Size = new System.Drawing.Size(584, 255);
+            this.rtbReceiveWindow.TabIndex = 0;
+            this.rtbReceiveWindow.Text = "";
+            // 
+            // rtbTransmitWindow
+            // 
+            this.rtbTransmitWindow.Location = new System.Drawing.Point(168, 288);
+            this.rtbTransmitWindow.Name = "rtbTransmitWindow";
+            this.rtbTransmitWindow.Size = new System.Drawing.Size(584, 44);
+            this.rtbTransmitWindow.TabIndex = 1;
+            this.rtbTransmitWindow.Text = "";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(32, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 303);
+            this.listBox1.TabIndex = 2;
+            // 
+            // MenuStrip1
+            // 
+            this.MenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filesToolStripMenuItem,
+            this.messageToolStripMenuItem,
+            this.attachmentsToolStripMenuItem,
+            this.logsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.MenuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip1.Name = "MenuStrip1";
+            this.MenuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.MenuStrip1.TabIndex = 3;
+            this.MenuStrip1.Text = "menuStrip1";
+            // 
+            // filesToolStripMenuItem
+            // 
+            this.filesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.filesToolStripMenuItem.Text = "Files";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // messageToolStripMenuItem
+            // 
+            this.messageToolStripMenuItem.Name = "messageToolStripMenuItem";
+            this.messageToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.messageToolStripMenuItem.Text = "Message";
+            // 
+            // attachmentsToolStripMenuItem
+            // 
+            this.attachmentsToolStripMenuItem.Name = "attachmentsToolStripMenuItem";
+            this.attachmentsToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.attachmentsToolStripMenuItem.Text = "Attachments";
+            // 
+            // logsToolStripMenuItem
+            // 
+            this.logsToolStripMenuItem.Name = "logsToolStripMenuItem";
+            this.logsToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.logsToolStripMenuItem.Text = "Logs";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.aboutToolStripMenuItem.Text = "About...";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.rtbTransmitWindow);
+            this.Controls.Add(this.rtbReceiveWindow);
+            this.Controls.Add(this.MenuStrip1);
+            this.MainMenuStrip = this.MenuStrip1;
+            this.Name = "Main";
+            this.Text = "ARES Messenger";
+            this.MenuStrip1.ResumeLayout(false);
+            this.MenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbReceiveWindow;
+        private System.Windows.Forms.RichTextBox rtbTransmitWindow;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.MenuStrip MenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem messageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem attachmentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+

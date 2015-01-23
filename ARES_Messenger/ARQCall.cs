276459a50b8b0906105b4690bb2fbdc176461601
Matthew <n8ohu@yahoo.com>
2015-01-23 18:23:16 -0500

    Added new forms for chat client

---
+++ ARES_Messenger/ARQCall.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ARES_Messenger
+{
+    public partial class ARQCall : Form
+    {
+        public ARQCall()
+        {
+            InitializeComponent();
+        }
+    }
+}
